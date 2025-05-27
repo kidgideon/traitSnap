79a8d45ec53ba8bdd3a1d49ebbffdcf5298e12f9
Gideon Kofi <kofi>
2025-05-27 17:03:45 +0100

    ADD quetions

--- src/assets/Home.jsx
+++ src/assets/Home.jsx
@@ -1,8 +1,17 @@
+ import "./styles/home.css"
+ import logo from "../../images/nobglogo.png"
 
-const Home = () => {
-    return(<div>
-   <h1>hi trait people</h1>
+ const Home = () => {
+    return(<div className="homePage"> 
+    <div className="home-icon-area">
+        <img src={logo} alt="" /> <h3>TraitSnap</h3>
+    </div>
+   <h1 className="homepage-into-text">Wrap your personality in one card</h1>
+ 
+<div className="startButton">
+    <button>Get Trait Card</button>
+</div>
     </div>)
-}
+}  
 
 export default Home
