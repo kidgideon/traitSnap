35f965b5c63fbf47f988ee59e274c0f807b89fd5
Gideon <156165263+kidgideon@users.noreply.github.com>
2025-06-15 19:13:30 +0100

    Create add2.jsx

---
+++ src/assets/add2.jsx
@@ -0,0 +1,50 @@
+import { useEffect, useRef } from "react";
+
+const inlineBannerTwo = () => {
+  const adRef = useRef(null);
+
+  useEffect(() => {
+    window.atOptions = {
+      key: "8a5e2af560aca5053ce2806d6b3bd6e7",
+      format: "iframe",
+      height: 50,
+      width: 320,
+      params: {},
+    };
+
+    const script = document.createElement("script");
+    script.type = "text/javascript";
+    script.src =
+      "//www.highperformanceformat.com/8a5e2af560aca5053ce2806d6b3bd6e7/invoke.js";
+    script.async = true;
+
+    script.onerror = () => {
+      console.warn("CompactBanner320x50 failed to load.");
+    };
+
+    if (adRef.current) {
+      adRef.current.appendChild(script);
+    }
+
+    return () => {
+      if (adRef.current) {
+        adRef.current.innerHTML = "";
+      }
+    };
+  }, []);
+
+  return (
+    <div
+      ref={adRef}
+      style={{
+        width: 320,
+        height: 50,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    />
+  );
+};
+
+export default inlineBannerTwo;
