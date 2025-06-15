ebc52d1445eb95fc616656c5f748941fd9d2f299
Gideon <156165263+kidgideon@users.noreply.github.com>
2025-06-15 19:33:28 +0100

    Update ad1.jsx: still fixing

--- src/assets/ad1.jsx
+++ src/assets/ad1.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useRef } from "react";
 
-const inlineBannerOne = () => {
+const InlineBannerOne = () => {
   const adRef = useRef(null);
 
   useEffect(() => {
@@ -47,4 +47,4 @@ const inlineBannerOne = () => {
   );
 };
 
-export default inlineBannerOne;
+export default InlineBannerOne;
